e5ee51d0a5899e9753360d545d679582d10638e2
Avery.Davenport <averymdavenport@gmail.com>
2021-07-13 12:37:27 -0600

    added guessing game

--- Materials/weather.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-api_key = 'e2479bf1d2fe9248e4908cdb4ec90cdd'
-city = "Orlando"
-url = 'http://api.openweathermap.org/data/2.5/weather?q='+city+'R&appid='+api_key
-
-request = requests.get(url)
-json = request.json()
-print(json)
